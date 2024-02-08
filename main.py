9739e28b070c921e591cf9124d60f2bf770f9e02
totof99 <totof@nomail.com>
2024-02-08 03:53:59 +0100

    add timer

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
@@ -21,7 +23,8 @@ def print_progress_bar(current, total, length=100):
 filePath = "documents/basel_minimum_capital_requirements_for_market_risk.pdf"
 ollama_url="http://localhost:11434"
 
-print("Loading " + filePath)
+print(f"Loading {filePath}")
+start_time = time.perf_counter()
 loader = PyPDFLoader(filePath)
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=500, chunk_overlap=50,
@@ -29,7 +32,8 @@ text_splitter = RecursiveCharacterTextSplitter(
     add_start_index=True
 )
 documents = loader.load_and_split(text_splitter=text_splitter)
-print("Loaded " + str(len(documents)) + " chunks from PDF " + filePath)
+elapsed_time = time.perf_counter() - start_time
+print(f"Loaded {len(documents)} chunks in {elapsed_time:0.1f}s from PDF {filePath}")
 
 ids = [str(uuid.uuid5(uuid.NAMESPACE_DNS, doc.page_content)) for doc in documents]
 
@@ -38,7 +42,7 @@ for doc, id in zip(documents, ids):
     if id not in unique_documents_ids:
         unique_documents_ids[id] = doc
 
-print("Found " + str(len(unique_documents_ids)) + " unique chunks")
+print(f"Found {len(unique_documents_ids)} unique chunks")
 
 oembed = OllamaEmbeddings(base_url=ollama_url, model="mistral", embed_instruction="", query_instruction="")
 vectorstore = Chroma(embedding_function=oembed, persist_directory="chroma")
@@ -46,6 +50,7 @@ vectorstore = Chroma(embedding_function=oembed, persist_directory="chroma")
 total_chunks = len(unique_documents_ids)
 chunks_treated = 0
 
+start_time = time.perf_counter()
 print_progress_bar(chunks_treated, total_chunks)
 for docs_with_ids in batched(unique_documents_ids.items(), 10):
     nb_docs = len(docs_with_ids)
@@ -64,7 +69,8 @@ for docs_with_ids in batched(unique_documents_ids.items(), 10):
     chunks_treated += nb_docs
     print_progress_bar(chunks_treated, total_chunks)
 
-print("Vector database filled with embeddings")
+elapsed_time = time.perf_counter() - start_time
+print(f"Vector database filled with embeddings in {elapsed_time:0.1f}s")
 
 ollama = Ollama(base_url=ollama_url, model="mistral")
 
